274b7b50922c350db48a8394dac44ad04d6c055f
GGas23 <giuseppe.eto@gmail.com>
2024-07-23 14:59:48 +0200

    Single docker-compose + initial data

--- climate-mq/climatemq/models.py
+++ climate-mq/climatemq/models.py
@@ -1,7 +1,7 @@
 from django.contrib.gis.db import models
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=False)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:

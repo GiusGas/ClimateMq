dba8c2b15ae1ee03d78d07f6b6c67d75419f3fe4
GGas23 <giuseppe.eto@gmail.com>
2024-07-30 10:26:36 +0200

    Last fix start script

--- climate-mq/start.sh
+++ climate-mq/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-wait-for-it postgres:5432 -- python manage.py collectstatic --noinput
-python manage.py migrate
+wait-for-it postgres:5432 -- python manage.py migrate
 python manage.py loaddata ./climatemq/fixtures/dump.json
 python manage.py runserver 0.0.0.0:8000 --settings=geodjango.settings
